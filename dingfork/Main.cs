ef3115b407b13bb67ce62b34bb9d5a92de64d6f2
Mysphyt <Aaron.collins3@gmail.com>
2025-08-28 14:04:45 -0500

    Update Main.cs

--- dingfork/Main.cs
+++ dingfork/Main.cs
@@ -1,40 +1,5 @@
 ﻿/*
     WingDingFork
-
-    Next:
-        * Clean up TODOs
-        
-        * Update to use the WingDings as the actual instructions in code
-            - Create mapping file instead of hard-coding instruction dict
-
-                [keymap.csv]
-                    "]", "🗀  "
-                    "[", "🗁  "
-                    "<", "👈︎ "
-                    ">", "👉︎ "
-                    ".", "🖳  "
-                    ",", "✂  "
-                    "+", "👆︎ "
-                    "-", "👇︎ "
-            
-        * Add sub-routines as text files with WingDing names
-            - Add ability to save current code as a new sub-routine
-
-                WingDingFork/subroutines/
-                    🕿.txt
-                    🖏.txt
-                    
-                [🕿.txt]
-                    >++++++++[<+++++++++>-]<.
-                    >++++[<+++++++>-]<+.
-                    +++++++..
-                    +++.
-                    >>++++++[<+++++++>-]<++.
-                    ------------.>++++++[<+++++++++>-]<+.
-                    <.+++.------.
-                    --------.
-                    >>>++++[<++++++++>-]<+.
-            
 */
 
 using System;
